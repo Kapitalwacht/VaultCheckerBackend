78683a53992f2ec2ace9e573b2b5d4483d07ae80
DoomerGX <josepeirano8@gmail.com>
2026-09-20 12:19:06 -0500

    docs(credit): remove RateType javadoc comment

--- src/main/java/com/vaultchecker/platform/credit/domain/model/valueobjects/RateType.java
+++ src/main/java/com/vaultchecker/platform/credit/domain/model/valueobjects/RateType.java
@@ -1,8 +1,5 @@
 package com.vaultchecker.platform.credit.domain.model.valueobjects;
 
-/**
- * Whether a pactada interest rate is expressed as a nominal or an effective rate.
- */
 public enum RateType {
     NOMINAL,
     EFFECTIVE
